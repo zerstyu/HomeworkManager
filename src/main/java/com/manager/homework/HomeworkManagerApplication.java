786155f4bf84dc290af3ff251be126319a03d9f6
Jae Hong <o_oloveo@naver.com>
2020-07-07 21:22:39 +0900

    first commit

---
+++ src/main/java/com/manager/homework/HomeworkManagerApplication.java
@@ -0,0 +1,13 @@
+package com.manager.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manager/homework/HomeworkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
