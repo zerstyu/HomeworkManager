a77231adebfcecdf6c052561cfc2663f8c1f344c
chudaeyoun <chudaeyoun@wemakeprice.com>
2020-07-09 22:56:36 +0900

    starter-web 추가

--- src/main/java/com/manager/homework/controller/UserController.java
+++ src/main/java/com/manager/homework/controller/UserController.java
@@ -1,5 +1,24 @@
 package com.manager.homework.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
-    // test commit
+
+    @PostMapping("/login")
+    public ResponseEntity<String> receiveMoney(@RequestBody String str, HttpServletRequest request) throws Exception {
+        log.info("Request parameter : {}", str);
+        return ResponseEntity.ok("success");
+    }
 }
