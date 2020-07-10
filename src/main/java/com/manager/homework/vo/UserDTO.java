78d1e48f4b99ac2f17a172151e6d3bcfc94a501d
chudaeyoun <chudaeyoun@naver.com>
2020-07-11 01:55:44 +0900

    userDTO json 설정

--- src/main/java/com/manager/homework/vo/UserDTO.java
+++ src/main/java/com/manager/homework/vo/UserDTO.java
@@ -1,7 +1,13 @@
 package com.manager.homework.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
 @ToString
 public class UserDTO {
     String id;
