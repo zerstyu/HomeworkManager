537b7350e017fdc072ecee0618b087fcd8e8591c
chudaeyoun <chudaeyoun@naver.com>
2020-07-21 01:31:31 +0900

    null 변수 response 에서 제거

--- src/main/java/com/manager/homework/vo/SubjectDto.java
+++ src/main/java/com/manager/homework/vo/SubjectDto.java
@@ -1,11 +1,13 @@
 package com.manager.homework.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.manager.homework.domain.Subject;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 @RequiredArgsConstructor
 @Data
