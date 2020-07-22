bb668a27b45cef572cd15b60c72341cd5310dbe4
chudaeyoun <chudaeyoun@naver.com>
2020-07-23 01:45:21 +0900

    redirect path는 다시 수정

--- src/main/java/com/manager/homework/controller/MemberController.java
+++ src/main/java/com/manager/homework/controller/MemberController.java
@@ -29,7 +29,7 @@ public class MemberController {
     public String execSignup(MemberDto memberDto) {
         memberService.joinUser(memberDto);
 
-        return "redirect:user/login";
+        return "redirect:/user/login";
     }
 
     // 로그인 페이지
