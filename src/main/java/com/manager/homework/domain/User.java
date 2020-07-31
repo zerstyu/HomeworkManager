d1dddb25f8d229e375c3a552187fe945d15b09f3
Jae Hong <o_oloveo@naver.com>
2020-07-31 22:07:13 +0900

    onetomany 제거

--- src/main/java/com/manager/homework/domain/User.java
+++ src/main/java/com/manager/homework/domain/User.java
@@ -6,9 +6,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -35,8 +36,4 @@ public class User extends BaseEntity {
 
     @Column
     private Gender gender;
-
-    @OneToMany(fetch=FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name="USER_ID")
-    private List<Subject> subjectList;
 }
