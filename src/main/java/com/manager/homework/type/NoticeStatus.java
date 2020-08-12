05d21b9959a5e64cc590d47d83dae38a2235378a
Jae Hong <o_oloveo@naver.com>
2020-08-12 18:59:40 +0900

    enum 값 변경 및 추가

--- src/main/java/com/manager/homework/type/NoticeStatus.java
+++ src/main/java/com/manager/homework/type/NoticeStatus.java
@@ -7,8 +7,11 @@ import lombok.Getter;
 @Getter
 // 공지 상태
 public enum NoticeStatus {
-    PENDING,
-    PROGRESS,
-    COMPLETED,
-    CANCELED
+    PENDING("대기"),
+    PROGRESS("진행중"),
+    GRADING("채점중"),
+    COMPLETED("완료"),
+    CANCELED("취소");
+
+    private final String noticeStatus;
 }
