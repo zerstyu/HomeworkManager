8556c948f5e08824dffac93913682419f6d6d279
chudaeyoun <chudaeyoun@naver.com>
2020-12-07 19:31:20 +0900

    groovy 테스트코드 주석

--- src/test/groovy/com/manager/homework/repository/SubjectRepositoryImplTest.groovy
+++ src/test/groovy/com/manager/homework/repository/SubjectRepositoryImplTest.groovy
@@ -1,80 +1,80 @@
-package com.manager.homework.repository
-
-import com.google.common.collect.Lists
-import com.google.common.collect.Maps
-import com.manager.homework.domain.Subject
-import com.manager.homework.domain.User
-import com.manager.homework.type.Gender
-import org.springframework.context.annotation.Profile
-import spock.lang.Specification
-
-import java.time.LocalDate
-
-@Profile("local")
-//@DataJpaTest
-class SubjectRepositoryImplTest extends Specification {
-//    @Autowired
-//    UserRepository userRepository
-//    @Autowired
-//    SubjectRepositoryImpl subjectRepositoryImpl
-//    @Autowired
-//    SubjectRepository subjectRepository
+//package com.manager.homework.repository
 //
-//    def "test 본인이 생성한 과목 리스트 조회"() {
-//        given:
-//        List<User> userList = userRepository.saveAll(getUserList())
-//        List<Subject> subjectList = subjectRepository.saveAll(getSubjectList())
+//import com.google.common.collect.Lists
+//import com.google.common.collect.Maps
+//import com.manager.homework.domain.Subject
+//import com.manager.homework.domain.User
+//import com.manager.homework.type.Gender
+//import org.springframework.context.annotation.Profile
+//import spock.lang.Specification
 //
-//        userList.forEach({ it -> println(it) })
-//        subjectList.forEach({ it -> println(it) })
+//import java.time.LocalDate
 //
-//        when:
-//        List<SearchSubjectDto> searchSubjectDtoList = subjectRepositoryImpl.selectAllSubjectList(1)
+//@Profile("local")
+////@DataJpaTest
+//class SubjectRepositoryImplTest extends Specification {
+////    @Autowired
+////    UserRepository userRepository
+////    @Autowired
+////    SubjectRepositoryImpl subjectRepositoryImpl
+////    @Autowired
+////    SubjectRepository subjectRepository
+////
+////    def "test 본인이 생성한 과목 리스트 조회"() {
+////        given:
+////        List<User> userList = userRepository.saveAll(getUserList())
+////        List<Subject> subjectList = subjectRepository.saveAll(getSubjectList())
+////
+////        userList.forEach({ it -> println(it) })
+////        subjectList.forEach({ it -> println(it) })
+////
+////        when:
+////        List<SearchSubjectDto> searchSubjectDtoList = subjectRepositoryImpl.selectAllSubjectList(1)
+////
+////        then:
+////        noExceptionThrown()
+////        println "------"
+////        searchSubjectDtoList.forEach({ it -> println(it) })
+////    }
 //
-//        then:
-//        noExceptionThrown()
-//        println "------"
-//        searchSubjectDtoList.forEach({ it -> println(it) })
+//    def getUserList() {
+//        List<User> userList = Lists.newArrayList()
+//        userList.add(
+//                User.builder()
+//                        .email("admin")
+//                        .password("admin")
+//                        .name("admin")
+//                        .groupName("HomeworkManager")
+//                        .birthday(LocalDate.of(1989, 8, 12))
+//                        .gender(Gender.MAN)
+//                        .build())
+//
+//        return userList
 //    }
-
-    def getUserList() {
-        List<User> userList = Lists.newArrayList()
-        userList.add(
-                User.builder()
-                        .email("admin")
-                        .password("admin")
-                        .name("admin")
-                        .groupName("HomeworkManager")
-                        .birthday(LocalDate.of(1989, 8, 12))
-                        .gender(Gender.MAN)
-                        .build())
-
-        return userList
-    }
-
-    def getSubjectList() {
-        List<Subject> subjectList = Lists.newArrayList()
-
-        subjectList.add(
-                Subject.builder()
-                        .user(getUserList().get(0))
-                        .name("수학")
-                        .inviteCode("aaaaabbbbb")
-                        .build())
-
-        return subjectList
-    }
-
-    def "Make range LinkHashMap"() {
-        given:
-        Map<String, Integer> map = Maps.newLinkedHashMap()
-
-        for (int i = 0; i < 10; i++) {
-            map.put((i * 10) + "-" + (i * 10 + 10), 0)
-        }
-
-        expect:
-
-        map.keySet().forEach(System.out.&println)
-    }
-}
+//
+//    def getSubjectList() {
+//        List<Subject> subjectList = Lists.newArrayList()
+//
+//        subjectList.add(
+//                Subject.builder()
+//                        .user(getUserList().get(0))
+//                        .name("수학")
+//                        .inviteCode("aaaaabbbbb")
+//                        .build())
+//
+//        return subjectList
+//    }
+//
+//    def "Make range LinkHashMap"() {
+//        given:
+//        Map<String, Integer> map = Maps.newLinkedHashMap()
+//
+//        for (int i = 0; i < 10; i++) {
+//            map.put((i * 10) + "-" + (i * 10 + 10), 0)
+//        }
+//
+//        expect:
+//
+//        map.keySet().forEach(System.out.&println)
+//    }
+//}
--- src/test/groovy/com/manager/homework/service/categoryTest.groovy
+++ src/test/groovy/com/manager/homework/service/categoryTest.groovy
@@ -1,47 +1,47 @@
-package com.manager.homework.service
-
-
-import com.manager.homework.type.CategoryGroupType
-import com.manager.homework.type.CategoryType
-import com.manager.homework.vo.CategoryGroupDto
-import spock.lang.Specification
-
-class categoryTest extends Specification {
-
-    def "카테고리 Type 으로 카테고리 그룹 정보 가져오기"() {
-        given:
-        CategoryType categoryType = TYPE
-
-        when:
-        CategoryGroupType categoryGroup = CategoryGroupType.findByCategoryCode(categoryType)
-
-        println "categoryGroup = " + categoryGroup.name()
-        println "categoryGroup = " + categoryGroup.getCategoryGroup()
-        println "categoryGroup = " + CategoryType.MATH.getCategory()
-
-        then:
-        categoryGroup.name() == GROUP.name()
-        noExceptionThrown()
-
-        where:
-        TYPE               | GROUP
-        CategoryType.MATH  | CategoryGroupType.STUDY
-        CategoryType.PHOTO | CategoryGroupType.ART
-
-    }
-
-    def "카테고리 리스트 가져오기"() {
-        given:
-        SubjectService subjectService = new SubjectService(null, null)
-
-        when:
-        List<CategoryGroupDto> categoryGroupDtoList = subjectService.getCategoryList()
-
-        categoryGroupDtoList.stream()
-                .forEach({ it -> println it })
-
-        then:
-        categoryGroupDtoList.size() > 0
-        categoryGroupDtoList[0].getCategoryList().size() > 0
-    }
-}
+//package com.manager.homework.service
+//
+//
+//import com.manager.homework.type.CategoryGroupType
+//import com.manager.homework.type.CategoryType
+//import com.manager.homework.vo.CategoryGroupDto
+//import spock.lang.Specification
+//
+//class categoryTest extends Specification {
+//
+//    def "카테고리 Type 으로 카테고리 그룹 정보 가져오기"() {
+//        given:
+//        CategoryType categoryType = TYPE
+//
+//        when:
+//        CategoryGroupType categoryGroup = CategoryGroupType.findByCategoryCode(categoryType)
+//
+//        println "categoryGroup = " + categoryGroup.name()
+//        println "categoryGroup = " + categoryGroup.getCategoryGroup()
+//        println "categoryGroup = " + CategoryType.MATH.getCategory()
+//
+//        then:
+//        categoryGroup.name() == GROUP.name()
+//        noExceptionThrown()
+//
+//        where:
+//        TYPE               | GROUP
+//        CategoryType.MATH  | CategoryGroupType.STUDY
+//        CategoryType.PHOTO | CategoryGroupType.ART
+//
+//    }
+//
+//    def "카테고리 리스트 가져오기"() {
+//        given:
+//        SubjectService subjectService = new SubjectService(null, null)
+//
+//        when:
+//        List<CategoryGroupDto> categoryGroupDtoList = subjectService.getCategoryList()
+//
+//        categoryGroupDtoList.stream()
+//                .forEach({ it -> println it })
+//
+//        then:
+//        categoryGroupDtoList.size() > 0
+//        categoryGroupDtoList[0].getCategoryList().size() > 0
+//    }
+//}
